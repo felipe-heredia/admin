c435689ea493ffe27d85baa06e27291350a0b92a
felipesuri <felipe@felipesuri.com>
2021-08-20 18:33:26 -0300

    Modifing Global and SEO

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1339),
+  port: env.int("PORT", 1338),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "87d021301faf0186ebbb09b39975f015"),
